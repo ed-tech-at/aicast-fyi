c673e793de643f3c61c257f4e5e6a65a3df1bc16
Bruenner <bruenner@tugraz.at>
2025-03-18 09:28:17 +0100

    favicon icon

---
+++ sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { onMount } from 'svelte';
+</script>
+
+<svelte:head>
+  <link rel="icon" href="/logo/aicast_v1.png" type="image/png" />
+  <link rel="shortcut icon" href="/logo/aicast_v1.png" type="image/png" />
+</svelte:head>
+
+<slot />
