e767ffa6a72b31b588327eb23757186fd737ee9a
bruenner <ben@bruenner.at>
2025-05-18 11:53:59 +0200

    Cleanup

--- sveltekit/src/routes/admin/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import '../main.css'; // Reintroducing the studio.css for styling
-</script>
-
-
-
-<main>
-  <slot /> <!-- This renders the page content inside /studio/ -->
-</main>
--- sveltekit/src/routes/admin/+page.svelte
+++
@@ -1,4 +0,0 @@
-<h1>Admin </h1>
-
-<a href="/admin/track">Tracks</a>
-<a href="/admin/elevenlabs">Elevenlabs</a>
